ccd4456d1efd28d80b1f58f16462882187be9e5c
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-03-19 19:29:21 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
+      <div className="App container">
+        <h1> Weather App</h1>
+        <footer>
+          This page was made by Nadira-Begim and is {""}
+          <a href="https://github.com/Nadi2894/my-react-weather-app">
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
